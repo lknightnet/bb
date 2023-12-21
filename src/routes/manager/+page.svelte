42b88e46efb442d6486b67e3e4fbbba02959fd65
jksgvj <regina.hisymutdinova@yandex.ru>
2023-12-21 20:51:07 +0500

    -

--- src/routes/manager/+page.svelte
+++ src/routes/manager/+page.svelte
@@ -35,19 +35,46 @@
     <Subname>Task Manager</Subname>
   
     <div class="rectangle-block">
-        <a href="#" on:click={clickToDivOne}>
-            <center>{nameProjectOne}</center>
-            <p>
-                {descriptionProjectOne}
-            </p>
-
-            <a href="#"><img class="gear" src="/gear.png" style="width: 30px; heigh:30px"></a>
+        <a href="#"><img class="gear" src="/gear.png" style="width: 30px; heigh:30px">
+            <div>
+                <div id="zatemnenie">
+                    <div id="okno">
+                      <a href="#" class="close">×</a>
+                      ID
+                    </div>
+                </div>
+                <a href="#" on:click={clickToDivOne}>
+                    <center>{nameProjectOne}</center>
+                    <p>
+                        Описание украли кролики
+                    </p>
+                </a>
+                  <a href="#zatemnenie">Свойства</a>
+            </div>
         </a>
+        
     </div>
 
     {#if show === true}
 
-        <div class="notes">ЗДЕСЬ ПИСАТЬ ЭТУ ТАБЛИЧКУ.</div>
+        <div class="notes">
+            <div class="notes-text" style="font: 20px 'Fira Sans', sans-serif">
+                Заметки
+            </div>
+
+            
+            <button type="submit" class="btn-add">Добавить заметки</button>
+            <button type="submit" class="btn-delete">Удалить заметки</button>
+            
+            <div class="tasks">
+                <ul class="task-list">
+                    <li><div class="square"></div></li>
+                    <li><div class="square"></div></li>
+                    <li><div class="square"></div></li>
+                    <li><div class="square"></div></li>
+                </ul>
+            </div>
+        </div>
 
     {:else if show === false}
 
@@ -62,25 +89,26 @@
         </div>
 
     {/if}
+    
 </div>
-<!-- <div>
-    <div id="zatemnenie">
-        <div id="okno">
-          <a href="#" class="close">×</a>
-          ID
-        </div>
-      </div>
-       
-      <a href="#zatemnenie">Свойства</a>
-</div> -->
-
 
 <style>
+    a {
+        text-decoration: none;
+    }
+    a:visited {
+        color: white;
+    }
+    a:active {
+        color: #629C5D;
+    }
+
     .gear{
         position: relative;
-        top: -30px;
+        top: -10px;
         left: 220px;
     }
+
     .and {
         font-size: 90px;
     }
@@ -98,6 +126,19 @@
         margin:50px 0 0 90px;
         border-radius: 15px;
     }
+    .notes {
+        width:950px;
+        height:500px;
+        padding:30px 20px;
+        background:#BBE7B7;
+        border: 1px solid;
+        color:#000;
+        text-align:left;
+        font-size:20px;
+        line-height:1.8;
+        margin:-562px 0 0 440px;
+        border-radius: 15px;
+    }
 
     .rectangle-block1 {
         width:250px;
@@ -112,7 +153,6 @@
         margin:-562px 0 0 440px;
         border-radius: 15px;
     }
-
     .rectangle-block2 {
         width:250px;
         height:500px;
@@ -126,7 +166,6 @@
         margin:-562px 0 0 790px;
         border-radius: 15px;
     }
-
     .rectangle-block3 {
         width:250px;
         height:500px;
@@ -152,7 +191,49 @@
         height: 40px;
         border: none;
     }
-    /* #zatemnenie {
+
+    .btn-add{
+        border-radius: 10px;
+        border: none;
+        color: #fff;
+        display: block;
+        width: 200px;
+        text-align: center;
+        font-size: 15px;
+        padding: 8px 20px;
+        margin: -25px 0 0 130px;
+        text-decoration: none;
+        background-color: #629C5D;
+    }
+    .btn-delete {
+        margin: -35px 0 0 350px;
+        border-radius: 10px;
+        border: none;
+        color: #fff;
+        display: block;
+        width: 200px;
+        text-align: center;
+        font-size: 15px;
+        padding: 8px 20px;
+        text-decoration: none;
+        background-color: #629C5D;
+    }
+
+    ul.task-list{
+        list-style: none;
+    }
+    .square{
+        display: block;
+        margin: 20px 0 0 -10px;
+        width: 30px;
+        height: 30px;
+        background: white;
+        border: 1px solid black;
+        border-radius: 5px;
+        
+    }
+
+    #zatemnenie {
         background: rgba(102, 102, 102, 0.5);
         width: 100%;
         height: 100%;
@@ -160,6 +241,7 @@
         top: 0;
         left: 0;
         display: none;
+        backdrop-filter: blur(3px);
     }
     #okno {
         width: 500px;
@@ -175,12 +257,11 @@
         left: 0;
         margin: auto;
         background: #BBE7B7;
-        box-shadow: 0 0 10px #444;
+        box-shadow: 10px 10px 10px #505050;
     }
     #zatemnenie:target {
         display: block;
     }
-
     .close {
         display: inline-block;
         border: none;
@@ -196,6 +277,7 @@
         background: #629C5D;
         font-size: 20pt;
         cursor:pointer;
-    } */
+    }
+
 
 </style>
